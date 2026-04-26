e359a135f27b661dca6a1cd7b6c3603e9f410eb0
Hackinmac <pro3582blox@gmail.com>
2026-04-26 19:48:05 -0400

    Fix MediatekWifi.h: Replace plist with proper C++ header file

--- MediatekWifi.h
+++ MediatekWifi.h
@@ -1,51 +1,56 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>CFBundleDevelopmentRegion</key>
-	<string>en</string>
-	<key>CFBundleExecutable</key>
-	<string>$(EXECUTABLE_PATH)</string>
-	<key>CFBundleIdentifier</key>
-	<string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
-	<key>CFBundleInfoDictionaryVersion</key>
-	<string>6.0</string>
-	<key>CFBundleName</key>
-	<string>$(PRODUCT_NAME)</string>
-	<key>CFBundlePackageType</key>
-	<string>KEXT</string>
-	<key>CFBundleShortVersionString</key>
-	<string>1.0</string>
-	<key>CFBundleSignature</key>
-	<string>????</string>
-	<key>CFBundleVersion</key>
-	<string>1.0</string>
-	<key>IOKitPersonalities</key>
-	<dict>
-		<key>MediatekWifi</key>
-		<dict>
-			<key>CFBundleIdentifier</key>
-			<string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
-			<key>IOClass</key>
-			<string>MediatekWifi</string>
-			<key>IOMatchCategory</key>
-			<string>IODefaultMatchCategory</string>
-			<key>IOProviderClass</key>
-			<string>IOPCIDevice</string>
-			<key>IOPCIPrimaryMatch</key>
-			<string>0x79611014 0x79221014</string>
-			<key>IOProbeScore</key>
-			<integer>5000</integer>
-		</dict>
-	</dict>
-	<key>OSBundleLibraries</key>
-	<dict>
-		<key>com.apple.kpi.iokit</key>
-		<string>19.0.0</string>
-		<key>com.apple.kpi.libkern</key>
-		<string>19.0.0</string>
-	</dict>
-	<key>OSBundleRequired</key>
-	<string>Root</string>
-</dict>
-</plist>
+#ifndef MEDIATEK_WIFI_H
+#define MEDIATEK_WIFI_H
+
+#include <IOKit/IOService.h>
+#include <IOKit/pci/IOPCIDevice.h>
+#include <IOKit/IOWorkLoop.h>
+#include <IOKit/IOInterruptEventSource.h>
+
+// Mediatek vendor ID
+#define MEDIATEK_VENDOR_ID 0x14
+
+// Register offsets
+#define MT_HW_CHIPID 0x0000
+#define MT_WFDMA0_HOST_INT_ENA 0x1234
+
+class MediatekWifi : public IOPCIDevice {
+    OSDeclareDefaultStructors(MediatekWifi)
+    
+public:
+    // Lifecycle methods
+    virtual bool start(IOService *provider) override;
+    virtual void stop(IOService *provider) override;
+    virtual void free() override;
+    virtual IOService *probe(IOService *provider, SInt32 *score) override;
+    
+private:
+    // PCI configuration
+    bool enablePCI();
+    bool disablePCI();
+    
+    // Memory mapping
+    bool mapDeviceMemory();
+    bool unmapDeviceMemory();
+    
+    // Register access
+    uint32_t readRegister(uint32_t offset);
+    void writeRegister(uint32_t offset, uint32_t value);
+    uint32_t readRegisterL1(uint32_t offset);
+    void writeRegisterL1(uint32_t offset, uint32_t value);
+    
+    // Interrupt handling
+    bool setupInterrupts();
+    void handleInterrupt(IOInterruptEventSource *source, int count);
+    
+    // Firmware loading
+    bool loadFirmware();
+    
+    // Member variables
+    volatile void *hwBase;
+    IOMemoryMap *memoryMap;
+    IOWorkLoop *workLoop;
+    IOInterruptEventSource *intSrc;
+    uint32_t chipId;
+};
+
+#endif
